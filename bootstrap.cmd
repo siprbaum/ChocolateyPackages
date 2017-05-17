d88bbed1a4c9afffa5941e26f4f875dcb20408ff
Baumann <peter.baumann@gmail.com>
2017-05-17 09:54:02 +0200

    Add bootstrap.cmd to install chocolate and set a sensible choco configuration
    Set allowGlobalConfirmation to be not bothered about scripts and licenses

---
+++ bootstrap.cmd
@@ -0,0 +1,3 @@
+@echo off
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco feature enable -n allowGlobalConfirmation
