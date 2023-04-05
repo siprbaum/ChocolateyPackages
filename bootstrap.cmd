1b81418286bc4dc022f6783c7d222da2793993f1
Peter Baumann <peter.baumann@gmail.com>
2023-04-05 16:50:35 +0200

    Update bootstrap.cmd to install git, too

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -2,3 +2,6 @@
 REM @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 choco feature enable -n allowGlobalConfirmation
+
+REM Install Git
+choco install git.install --params "'/GitAndUnixToolsOnPath /WindowsTerminal /NoAutoCrlf /SChannel /WindowsTerminalProfile'"
